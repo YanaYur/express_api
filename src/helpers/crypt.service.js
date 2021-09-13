fff76f18262884a0c292e102de0f77b4da537dc9
Yana Yur <yana.yuryeva@gmail.com>
2021-09-13 20:42:51 +0200

    feat: added crypto service

--- src/helpers/crypt.service.js
+++ src/helpers/crypt.service.js
@@ -1,32 +1,30 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 async function crypt(data) {
-    return new Promise((resolve, reject) => {
-        bcrypt.hash(data, saltRounds, function (err, hash) {
-            if (err) {
-                return reject(err)
-            } else {
-                return resolve(hash)
-            }
-        });
-    })
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(data, saltRounds, function (err, hash) {
+      if (err) {
+        return reject(err);
+      } else {
+        return resolve(hash);
+      }
+    });
+  });
 }
 
-const compare = async (cripted, original) => new Promise((resolve, reject) => {
+const compare = async (cripted, original) =>
+  new Promise((resolve, reject) => {
     bcrypt.compare(original, cripted, function (err, result) {
-        debugger
-        if (err) {
-            reject(err)
-        } else {
-            resolve(result)
-        }
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
     });
-})
-
-
+  });
 
 module.exports = {
-    crypt,
-    compare
-}
+  crypt,
+  compare,
+};

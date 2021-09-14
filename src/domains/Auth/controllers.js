36d7020013a2ac61b611e8acdb918aa5dae85353
Yana Yur <yana.yuryeva@gmail.com>
2021-09-14 20:11:46 +0200

    feat: refactored auth domain

--- src/domains/Auth/controllers.js
+++ src/domains/Auth/controllers.js
@@ -1,29 +1,36 @@
 const UsersService = require("../Users/Service");
-const CryptoService = require("../../helpers/crypt.service");
+const TokenService = require("../../helpers/token.service");
 
-async function signIn(req, res, next) {
+async function signIn(req, res) {
   try {
     const { body } = req;
     const createdUser = await UsersService.createUser(body);
+
     delete createdUser.password;
+
     return res.status(201).json(createdUser);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 }
 
-async function logIn(req, res, next) {
+async function logIn(req, res) {
   try {
-    const { body } = req;
-    const { email, password } = body;
+    const { email, password } = req.body;
 
     const user = await UsersService.findByEmail(email);
-    if (!user) return res.status(404).json({ message: "User not found" });
-    const validated = await user.comparePassword(password);
-    if (!validated)
-      return res.status(403).json({ message: "Incorrect password" });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
 
-    res.status(200).json(user);
+    const validated = await user.comparePassword(password);
+    if (!validated) {
+      return res.status(403).json({ message: "Incorrect password" });
+    }
+
+    const token = TokenService.sign(user.toObject());
+
+    res.status(200).json({ token });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
--- src/domains/Auth/router.js
+++ src/domains/Auth/router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Router = express.Router();
 const Controllers = require("./Controllers");
 
-Router.post("/signin", Controllers.signIn).post("/login", Controllers.logIn);
+Router.post("/signin", Controllers.signIn);
+Router.post("/login", Controllers.logIn);
 
 module.exports = Router;

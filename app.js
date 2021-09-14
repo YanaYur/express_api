67fb9c53644cc4186eb15353e7bb6cc42596f781
Yana Yur <yana.yuryeva@gmail.com>
2021-09-14 20:12:39 +0200

    feat: updated app

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const server = require('./src/server')
 
 const PORT = process.env.PORT || 3001;
 
-server.config(app)
+(async () => {
 
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
-})
+  await server.config(app)
+
+  app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+  })
+})()
+
 
